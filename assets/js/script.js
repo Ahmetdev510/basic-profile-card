a4845e3440af208379300a82d90967a2b9e0bb86
Ahmetdev510 <76014530+Ahmetdev510@users.noreply.github.com>
2023-03-04 02:54:20 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,8 @@ const tag = document.querySelector(".container .box .discord-tag");
 let tagText;
 tag.addEventListener("click", () => {
     if(!tagText) tagText = tag.textContent;
-    tag.innerHTML = `<i class="fa-solid fa-circle-check"></i> Copied!`;
-    setTimeout(() => {
-        tag.innerHTML = tagText;
-    }, 3*1000);
+    navigator.clipboard.writeText(tagText).then(()=>{
+        tag.innerHTML = `<i class="fa-solid fa-circle-check"></i> Copied!`;
+        setTimeout(() => tag.innerHTML = tagText, 3*1000);
+    }).catch(err=>console.log(err));
 });
